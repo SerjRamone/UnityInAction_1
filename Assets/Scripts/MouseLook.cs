2fc3a9544f95d35ece1479be4e80930f663d2445
SerjRamone <sergey.greznov@gmail.com>
2018-06-05 23:46:17 +0300

    initial 2

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    //структура данных enum будет сопостовлять имена с параметрами
+    public enum RotationAxes {
+        MouseXAndY = 0,
+        MouseX = 1,
+        MouseY = 2
+    };
+
+	// Use this for initialization
+	void Start()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+		
+	}
+}
