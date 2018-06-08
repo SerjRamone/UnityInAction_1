91cf6d96a55e152af1e456c4919d6a568578a202
SerjRamone <sergey.greznov@gmail.com>
2018-06-08 23:06:16 +0300

    бросание луча

---
+++ Assets/Scripts/RayShooter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayShooter : MonoBehaviour
+{
+    private Camera _camera;
+
+	void Start()
+    {
+        //доступ к другим компонентам, присоединённым к этому же объекту
+        _camera = GetComponent<Camera>();
+	}
+	
+	void Update()
+    {
+		if (Input.GetMouseButtonDown(0)) //реакция на нажатие кнопки мыши
+        {
+            //середина экрана - это половина его высоты и ширины
+            Vector3 point = new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2, 0);
+
+            //создание в этой точке луча
+            Ray ray = _camera.ScreenPointToRay(point);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                //загружаем координаты точки в которую попал луч
+                Debug.Log("Hit " + hit.point);
+            }
+        }
+	}
+}
