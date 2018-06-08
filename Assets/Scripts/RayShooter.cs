45512442e5a42d0dff2426ff5bdec15b32dc8040
SerjRamone <sergey.greznov@gmail.com>
2018-06-08 23:27:54 +0300

    прицел + следы попаданий

--- Assets/Scripts/RayShooter.cs
+++ Assets/Scripts/RayShooter.cs
@@ -10,6 +10,10 @@ public class RayShooter : MonoBehaviour
     {
         //доступ к другим компонентам, присоединённым к этому же объекту
         _camera = GetComponent<Camera>();
+
+        //скрываем указатель мыши в центре экрана
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
 	}
 	
 	void Update()
@@ -25,9 +29,31 @@ public class RayShooter : MonoBehaviour
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
             {
-                //загружаем координаты точки в которую попал луч
-                Debug.Log("Hit " + hit.point);
+                //запуск корутина в ответ на попадание
+                StartCoroutine(SphereIndicator(hit.point));
             }
         }
 	}
+
+    //корутины пользуются функциями IEnumerator
+    private IEnumerator SphereIndicator(Vector3 pos)
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = pos;
+
+        //ключевое слово yield показывает корутину когда надо остановиться
+        yield return new WaitForSeconds(1);
+
+        //удаляем GameObject и очищаем память
+        Destroy(sphere);
+    }
+
+    private void OnGUI()
+    {
+        int size = 12;
+        float posX = _camera.pixelWidth / 2 - size / 4;
+        float posY = _camera.pixelHeight / 2 - size / 2;
+        //отображает на экране символ
+        GUI.Label(new Rect(posX, posY, size, size), "*");
+    }
 }
